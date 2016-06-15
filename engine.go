f9d762aec35512754f2dbcdab8e0261c0e2e8836
kai <kergin2016@gmail.com>
2016-06-15 12:34:40 -0400

    AI has access to more variables

--- engine.go
+++ engine.go
@@ -13,6 +13,7 @@ var mainChain [][]int
 var scoreOne int = 0
 var scoreTwo int = 0
 var move int = 1
+var player int = 0
 
 func main() {
 	var strike1 int = 0
@@ -66,6 +67,7 @@ func main() {
 	for mainLoop {
 		printBoard(sliceMain,showCoords)
 		if debug && revert2{
+			player = 1
 			fmt.Print("-> ")
 			text, _ := reader.ReadString('\n')
 			if text == "stop\n" {
@@ -115,6 +117,7 @@ func main() {
 			tempScore1 = scoreOne
 			tempScore2 = scoreTwo
 			tempMove = move
+			player = 2
 			
 			t0 = time.Now()
 			nextMove = returnMove(sliceMain)
